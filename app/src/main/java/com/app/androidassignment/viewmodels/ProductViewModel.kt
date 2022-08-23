adaf22a1da32425a81047940f2afaf83fb38b96b
vikalp <vikalp.vashisthQ@appventurez.com>
2022-08-23 15:41:27 +0530

    made changes for handling exception in product view model

--- app/src/main/java/com/app/androidassignment/viewmodels/ProductViewModel.kt
+++ app/src/main/java/com/app/androidassignment/viewmodels/ProductViewModel.kt
@@ -5,11 +5,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.app.androidassignment.helpers.CoroutineHandler
 import com.app.androidassignment.models.FavoriteModel
 import com.app.androidassignment.models.Hits
 import com.app.androidassignment.models.ProductModel
 import com.app.androidassignment.repositories.ProductRepository
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -21,26 +21,24 @@ class ProductViewModel : ViewModel() {
         ProductRepository()
     }
     var productList: ArrayList<Hits>? = null
+    private val handler = CoroutineExceptionHandler { _, throwable ->
+        println("Exception Caught $throwable")
+    }
 
     //used to fetch product list data from product endpoint
     fun getProductData(): LiveData<ProductModel?> {
         val productListMutableLiveData = MutableLiveData<ProductModel?>()
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val response = productRepository.getProductData()
-                withContext(Dispatchers.Main) {
-                    response?.let {
-                        if (it.isSuccessful) {
-                            productListMutableLiveData.value = it.body()
-                        } else {
-                            productListMutableLiveData.value = null
-                            Log.e(TAG, "getProductData: " + it.errorBody().toString())
-                        }
+        viewModelScope.launch(Dispatchers.IO.plus(handler)) {
+            val response = productRepository.getProductData()
+            withContext(Dispatchers.Main) {
+                response?.let {
+                    if (it.isSuccessful) {
+                        productListMutableLiveData.value = it.body()
+                    } else {
+                        productListMutableLiveData.value = null
+                        Log.e(TAG, "getProductData: " + it.errorBody().toString())
                     }
                 }
-            } catch (e: Exception) {
-                productListMutableLiveData.value = null
-                Log.e(TAG, "getProductData: " + e.printStackTrace())
             }
 
         }
@@ -50,22 +48,17 @@ class ProductViewModel : ViewModel() {
 
     fun favoriteThisItem(): LiveData<FavoriteModel> {
         val favoriteItem = MutableLiveData<FavoriteModel>()
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val response = productRepository.favoriteItem()
-                withContext(Dispatchers.Main) {
-                    response?.let {
-                        if (it.isSuccessful) {
-                            favoriteItem.value = it.body()
-                        } else {
-                            favoriteItem.value = null
-                            Log.e(TAG, "favoriteThisItem: " + it.errorBody().toString())
-                        }
+        viewModelScope.launch(Dispatchers.IO.plus(handler)) {
+            val response = productRepository.favoriteItem()
+            withContext(Dispatchers.Main) {
+                response?.let {
+                    if (it.isSuccessful) {
+                        favoriteItem.value = it.body()
+                    } else {
+                        favoriteItem.value = null
+                        Log.e(TAG, "favoriteThisItem: " + it.errorBody().toString())
                     }
                 }
-            } catch (e: Exception) {
-                favoriteItem.value = null
-                Log.e(TAG, "favoriteThisItem: " + e.printStackTrace())
             }
         }
         return favoriteItem
@@ -73,22 +66,17 @@ class ProductViewModel : ViewModel() {
 
     fun deleteThisItemFromFavorite(): LiveData<FavoriteModel> {
         val deletedItemFromFavorite = MutableLiveData<FavoriteModel>()
-        viewModelScope.launch(Dispatchers.IO.plus(CoroutineHandler.coroutineExceptionHandler)) {
-            try {
-                val response = productRepository.deleteItemFromFavorite()
-                withContext(Dispatchers.Main) {
-                    response?.let {
-                        if (it.isSuccessful) {
-                            deletedItemFromFavorite.value = it.body()
-                        } else {
-                            deletedItemFromFavorite.value = null
-                            Log.e(TAG, "favoriteThisItem: " + it.errorBody().toString())
-                        }
+        viewModelScope.launch(Dispatchers.IO.plus(handler)) {
+            val response = productRepository.deleteItemFromFavorite()
+            withContext(Dispatchers.Main) {
+                response?.let {
+                    if (it.isSuccessful) {
+                        deletedItemFromFavorite.value = it.body()
+                    } else {
+                        deletedItemFromFavorite.value = null
+                        Log.e(TAG, "favoriteThisItem: " + it.errorBody().toString())
                     }
                 }
-            } catch (e: Exception) {
-                deletedItemFromFavorite.value = null
-                Log.e(TAG, "favoriteThisItem: " + e.printStackTrace())
             }
         }
         return deletedItemFromFavorite
